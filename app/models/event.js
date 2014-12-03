d0e3201fc69651216e77e0cf272eca8fdd355bbf
Monomachus <monomachus85@yahoo.com>
2014-12-03 18:33:05 +0200

    First commit

---
+++ app/models/event.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    EventSchema = new Schema({
+        ratings: []
+    });
+
+
+EventSchema.pre('save', function (next) {
+    
+    var totalRatings = 0;
+    
+    for (var i = 0; i < this.ratings.length; i++) { 
+        totalRatings += this.ratings[i].rating;
+    }
+    
+    this.averageRating = totalRatings / this.ratings.length; 
+    
+    next();
+});
+
+module.exports = mongoose.model('Event', EventSchema);
